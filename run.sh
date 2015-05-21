fa5e1777ceb234caa591da7962c97af42c8dcf90
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-05-21 14:44:44 -0300

    Again...

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ source /opt/adapteva/esdk/setup.sh
 if [ ! -f /app/installed.txt ]; then
 	echo "Building FFT correlation"
 	cd /app/parallella-fft-xcorr && PATH=/usr/local/browndeer/bin:$PATH LD_LIBRARY_PATH=/usr/local/browndeer/lib:$LD_LIBRARY_PATH make
-	cp /app/parallella-fft-xcorr/libfft-demo.so /usr/local/lib/
+	install -m 644 /app/parallella-fft-xcorr/libfft-demo.so /usr/local/lib/
 	ldconfig -n /usr/local/lib
 
-	ls /usr/local/lib
-	
+	ls -l /usr/local/lib
+
 	echo "Installing app"
 	cd /app && npm install --unsafe-perm
 
