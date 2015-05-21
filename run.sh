a9fe3e0d8da112cb92b2e0cde63b2dbfe41b81ef
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-05-21 14:25:48 -0300

    ldconfig

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ ! -f /app/installed.txt ]; then
 	echo "Building FFT correlation"
 	cd /app/parallella-fft-xcorr && PATH=/usr/local/browndeer/bin:$PATH LD_LIBRARY_PATH=/usr/local/browndeer/lib:$LD_LIBRARY_PATH make
 	install /app/parallella-fft-xcorr/libfft-demo.so /usr/local/lib
-
+	ldconfig -n /usr/local/lib /usr/local/browndeer/lib
 	echo "Installing app"
 	cd /app && npm install --unsafe-perm
 
