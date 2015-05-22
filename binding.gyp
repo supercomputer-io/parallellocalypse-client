cebc3695931c8c21d292c82bafb9004f5329ba7f
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-05-22 12:05:39 -0300

    Use coprthr implementation of fft

--- binding.gyp
+++ binding.gyp
@@ -4,7 +4,7 @@
 			"target_name": "xcorr",
 			"sources": [ "lib/xcorr.cc" ],
 			"link_settings": {
-				"libraries": [ "-L/app/parallella-fft-xcorr", "-lfft-demo"]
+				"libraries": [ "-L/app/parallella-fft-xcorr", "-lfft-demo-coprthr"]
 			}
 		}
 	]
--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ source /opt/adapteva/esdk/setup.sh
 
 if [ ! -f /app/installed.txt ]; then
 	echo "Building FFT correlation"
-	cd /app/parallella-fft-xcorr && PATH=/usr/local/browndeer/bin:$PATH LD_LIBRARY_PATH=/usr/local/browndeer/lib:$LD_LIBRARY_PATH make
-	install -m 644 /app/parallella-fft-xcorr/libfft-demo.so /usr/lib/
+	cd /app/parallella-fft-xcorr && PATH=/usr/local/browndeer/bin:$PATH LD_LIBRARY_PATH=/usr/local/browndeer/lib:$LD_LIBRARY_PATH make IMPL=coprthr
+	install -m 644 /app/parallella-fft-xcorr/libfft-demo-coprthr.so /usr/lib/
 	ldconfig /usr/lib /usr/local/browndeer/lib
 
 	cp /app/parallella-fft-xcorr/device.cbin.3.e32 /app/
